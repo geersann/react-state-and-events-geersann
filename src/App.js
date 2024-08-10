006e240608c9e87faa6a4f7f3d8af970e8b05982
geersann <148795588+geersann@users.noreply.github.com>
2024-08-10 16:20:48 +0000

    add toggleCase func, for lower-upperCase

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React from 'react';
 import './App.css';
 
 export default class App extends React.Component {
-  
+  state = {
+    isUpperCase: false
+  };
+
+  toggleCase = () => {
+    this.setState(prevState => ({
+      isUpperCase: !prevState.isUpperCase,
+    }))
+  }
+
   render() {
-    return <div> React Marathon </div>    
+    const text = "React Marathon"
+    const displayText = this.state.isUpperCase ? text.toUpperCase() : text.toLowerCase();
+    return <div onClick={this.toggleCase}>
+      {displayText}
+    </div>    
   }
 }
